223dbad26a340ff3554302eb978688d2279e118a
LukasCaha <lukas.caha@outlook.com>
2019-11-09 13:55:37 +0100

    Aditional scripts

---
+++ Stonks/Plugins/Database/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stonks.Plugins.Database
+{
+    public class Database
+    {
+    }
+}
