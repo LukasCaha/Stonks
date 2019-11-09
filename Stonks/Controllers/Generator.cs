9e61ee802a95e65f4c6517b7e46e6c68159e8963
Tretygon <Tretygon+github@gmail.com>
2019-11-09 14:43:02 +0100

    improved Generator

--- Stonks/Controllers/Generator.cs
+++ Stonks/Controllers/Generator.cs
@@ -9,9 +9,25 @@ namespace Stonks.Controllers
     {
         static Random rng = new Random();
         static readonly double Constant = 0.05;
+
+        /// random number between (-1,1), gaussian distribution
+        static double RandGaussian() => rng.NextDouble() - rng.NextDouble();
+
+
         public static double RandomlyModify(double Value, double intensity = 1.0)
         {
-            double modifier = rng.NextDouble() - rng.NextDouble();   // random number between (-1,1), gaussian distribution
+            return (1 + RandGaussian() * Constant * intensity) * Value;
+
+        }
+        public static double RandomGrowth(double Value, double intensity = 1.0)
+        {
+            double modifier =  Math.Abs(RandGaussian());  
+            return (1 + modifier * Constant * intensity) * Value;
+
+        }
+        public static double RandomFall(double Value, double intensity = 1.0)
+        {
+            double modifier = -Math.Abs(RandGaussian());
             return (1 + modifier * Constant * intensity) * Value;
 
         }
